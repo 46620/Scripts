775812675c0e13b91542d1ee248d0d83ecc29c89
46620 <seanfjr02@gmail.com>
2020-08-21 21:50:44 -0400

    Create update-HAMA.sh
    Update HAMA for Plex

---
+++ update-HAMA.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# run with sudo, tested on ubuntu
+
+echo STOPPING PLEX
+systemctl stop plexmediaserver
+
+echo UPDATING HAMA
+cd '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
+git reset --hard && git pull
+chown -R plex:plex '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
+chmod 775 -R '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
+
+echo UPDATING Absolute Series Scanner
+wget -O '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners/Series/Absolute Series Scanner.py' https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py
+chown -R plex:plex '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners'
+chmod 775 -R '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners'
+
+echo STARTING PLEX
+systemctl restart plexmediaserver
