81821a570cec8c3a838979b72c1aa0f46ad33f12
46620 <seanfjr02@gmail.com>
2020-12-17 17:47:42 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 screen -wipe
+
 cd /opt/Minecraft
-rm archive.zip # If script was stopped while downloading the script this will prevent archive.zip.1 from happening
 wget "https://papermc.io/ci/job/Paper-1.16/lastSuccessfulBuild/artifact/*zip*/archive.zip"
 unzip archive.zip
 mv archive/paperclip.jar ./paper.jar
 rm -rf wget*
-rm -rf archive*
-java -jar paper.jar
+rm -rf archiv*
+java -jar paper.jar nogui
+systemctl --user restart minecraft.service
