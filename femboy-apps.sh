1602af74d24f8a4ae45814c4fdad4106e3b03451
46620 <seanfjr02@gmail.com>
2021-04-14 13:32:54 -0400

    Update and rename discord.sh to femboy-apps.sh
    Now I gotta update my fucking cron tab and update it locally

---
+++ femboy-apps.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# This script is just to pull the latest version of apps to my gitlab server to be patched with new stuff (or as a backup for old versions)
+
+############
+# Env shit #
+############
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin"
+
+###########
+# Discord #
+###########
+url=https://ws75.aptoide.com/api/7/app/getMeta?package_name=com.discord
+ver=`curl -sL $url | jq -r '.data.file.vercode'`
+
+cd /tmp
+curl -sL $url | jq -r '.data.file.path' | wget -i -
+mv *.apk com.discord.apk
+apktool d com.discord.apk
+git clone https://git.46620.moe/femboy-apps/discord/discord.git
+rm -rf discord/com.discord
+cp -r com.discord discord
+cd discord
+git add .
+git commit -a -m "update to $ver"
+git push
+cd ..
+rm -rf *discord*
+
+#############
+# Instagram #
+#############
+
+url=https://ws75.aptoide.com/api/7/app/getMeta?package_name=com.instagram.android
+ver=`curl -sL $url | jq -r '.data.file.vercode'`
+
+cd /tmp
+curl -sL $url | jq -r '.data.file.path' | wget -i -
+mv *.apk com.instagram.apk
+apktool d com.instagram.apk
+git clone https://git.46620.moe/femboy-apps/instagram/instagram.git
+rm -rf instagram/com.instagram
+cp -r com.instagram instagram
+cd instagram
+git add .
+git commit -a -m "update to $ver"
+git push
+cd ..
+rm *instagram*
