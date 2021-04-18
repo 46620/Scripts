89086d5e3ad8e54b8dc4a2519359046f6e3ce273
46620 <seanfjr02@gmail.com>
2021-04-18 18:09:16 -0400

    Create taisei-nx.sh
    Currently just builds and installs it to the switch folder inside the build directory. There will be an update soon that will zip it up for use on my jenkins server

---
+++ taisei-nx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script will assume you have all build deps install already, please install them before using this
+
+git clone --recursive https://github.com/taisei-project/taisei.git
+cd taisei
+mkdir -p ./build/nx
+./switch/crossfile.sh > ./build/nx/crossfile.txt
+meson --prefix=`pwd`/build/nx/switch --cross-file="./build/nx/crossfile.txt" . ./build/nx
+ninja -C ./build/nx
+ninja install -C ./build/nx
