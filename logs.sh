19b3a7eca31551ed169a9ab48345cdde0cfac216
46620 <46620osu@gmail.com>
2021-06-22 02:32:33 +0000

    Update log link

--- logs.sh
+++ logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://git.46620.moe/46620/mc-logs /tmp/logs/mc-logs
+git clone https://git.46620.moe/minecraft-server-shit/server-logs.git /tmp/logs/mc-logs
 cd /tmp/logs/
 mv /opt/Minecraft/logs/*.log.gz /tmp/logs
 gunzip *.log.gz
