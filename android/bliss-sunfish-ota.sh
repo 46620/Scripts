7357bf4d24115f337c15c8642cdd8f64a0e289d7
46620 <46620osu@gmail.com>
2021-07-19 17:56:59 -0400

    gapps

--- android/bliss-sunfish-ota.sh
+++ android/bliss-sunfish-ota.sh
@@ -7,22 +7,13 @@ today=$(date "+%Y%m%d")
 echo "Updating ROM source" #TODO: FUCKING MAKE THIS CLEANER YOU FUCKING DUMMY
 cd /opt/android/Bliss/r/
 repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
-cd device/google/sunfish
-git pull
-cd ../../..
-cd kernel/google/sunfish
-git pull
-cd ../../..
-cd vendor/google
-git pull
-cd ../..
 
 echo "Building rom with latest changes"
 . build/envsetup.sh
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
 ccache -M 100G
-blissify sunfish
+blissify -g sunfish
 
 echo "Pushing updates to update server"
 cp $OUT/bliss_sunfish-ota-eng.seanf.zip /var/www/LineageOTA/builds/full/bliss-14.2-$today-nightly-sunfish.zip
