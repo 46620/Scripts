cfd466cc3c929f44b050b2226436a95942c92976
46620 <46620osu@gmail.com>
2021-08-21 09:10:12 +0000

    Get working

--- cutthecord/custom builds/jenkins.sh
+++ cutthecord/custom builds/jenkins.sh
@@ -1,92 +1,83 @@
 #!/bin/bash
 
 # This is a rework of my old auto ctc script to support multiple builds
+# For some reason when I did this all in functions it decided to just be a fuck fest of race conditions
+# Branding, customversion, and eventually notrack and nonearby will be included in all builds by default
 
-user_set(){
-    user=`echo $JOB_NAME_BASE | cut -c 12-`
-    case "$user" in
-        46620)
-            CTCPATCHES=(betterdmheader customversion disable-mobileindicator noblocked squareavatars)
-            USE_BLOBS=1
-            CTCBRANCH=CutTheCord
-            CTCFORK=femboy
-            ;;
-        t3ch)
-            CTCPATCHES=(betterdmheader customversion disable-mobileindicator noblocked squareavatars)
-            USE_BLOBS=1
-            CTCNAME=Discord
-            CTCBRANCH=disnuts
-            ;;
-        *)
-            echo "This should literally not happen" >&2
-            exit 1
-    esac
-}
+user=`echo $JOB_NAME_BASE | cut -c 12-`
+case "$user" in
+    46620)
+        CTCPATCHES=(betterdmheader customversion disable-mobileindicator noblocked squareavatars)
+        USE_BLOBS=1
+        USE_MUTANT=0
+        CUSTOM_ICON=1
+        CTCBRANCH=CutTheCord
+        CTCFORK=femboy
+        ;;
+    t3ch)
+        CTCPATCHES=(betterdmheader)
+        USE_BLOBS=0
+        USE_MUTANT=0
+        CTCBRANCH=Discord
+        CTCFORK=disnuts
+    ;;
+    *)
+        echo "This should literally not happen" >&2
+        exit 1
+esac
 
-setup(){
-    mkdir tmp
-    cd tmp
-    git clone https://booba.tech/CutTheCord/cutthecord.git
-    git clone --depth=1 https://booba.tech/CutTheCord/discord.git
-    cp -r discord/com.discord .
-}
+mkdir tmp
+cd tmp
+CTCTOP=`pwd`
+CTCPATCHESPATH=$CTCTOP/cutthecord/patches
+CTCBASE=$CTCTOP/com.discord
 
-emotes(){
-    if [[ USE_BLOBS -eq 1 ]]
-    then
-        git clone https://booba.tech/CutTheCord/blobs.git
-        cd blobs
-        export DISTOK_EMOJI_BLOBMOJI=`pwd`
-        cd ..
-    elif [[ USE_MUTANT -eq 1 ]]
-    then
-        echo "Mutant is currently not supported."
-    fi
-}
+git clone https://booba.tech/CutTheCord/cutthecord.git
+git clone --depth=1 https://booba.tech/CutTheCord/discord.git
+cp -r discord/com.discord .
 
-patch() {
-    cd cutthecord
-    export ver=`cat patchport-state.json | jq -r .versioncode`
-    cd patches/branding
-    python addpatch.py $ver.patch $CTCNAME $CTCBRANCH
-    cd ../../..
-    cd com.discord
-    export DISTOK_EXTRACTED_DISCORD_PATH=`pwd`
-    for cum in ${CTCPATCHES[@]}
-        do
-    	    patch -p1 < "../cutthecord/patches/$cum/$ver.patch"
-    done
-    patch -p1 < "../cutthecord/patches/branding/$ver-custom.patch"
-    if [[ USE_BLOBS -eq 1 ]]
-    then
-        python "../cutthecord/patches/blobs/emojireplace.py"
-    elif [[ USE_MUTANT -eq 1 ]]
-    then
-        echo "Mutant is currently not supported."
-    fi
-}
-
-res(){
+if [[ USE_BLOBS -eq 1 ]]
+then
+    git clone https://booba.tech/CutTheCord/blobs.git
+    cd blobs
+    export DISTOK_EMOJI_BLOBMOJI=`pwd`
     cd ..
-    git clone https://booba.tech/CutTheCord/ctc.git -b $user
-    cd com.discord
-    cp -rv ../ctc/res .
+elif [[ USE_MUTANT -eq 1 ]]
+then
+    echo "Mutant is currently not supported."
+fi
+
+cd cutthecord
+export ver=`cat patchport-state.json | jq -r .versioncode`
+cd patches/branding
+python3 addpatch.py $ver.patch $CTCBRANCH $CTCFORK
+cd $CTCBASE
+export DISTOK_EXTRACTED_DISCORD_PATH=`pwd`
+for cum in ${CTCPATCHES[@]}
+    do
+    	patch -p1 < "../cutthecord/patches/$cum/$ver.patch"
+done
+
+patch -p1 < "../cutthecord/patches/branding/$ver-custom.patch"
+
+if [[ USE_BLOBS -eq 1 ]]
+then
+    python3 "../cutthecord/patches/blobs/emojireplace.py"
+elif [[ USE_MUTANT -eq 1 ]]
+then
+    echo "Mutant is currently not supported."
+fi
+
+cd ..
+git clone https://booba.tech/CutTheCord/ctc.git -b $user
+cd com.discord
+cp -rv ../ctc/res .
+
+if [[ CUSTOM_ICON -eq 1 ]]
+then
     bash "../cutthecord/patches/branding/customicon.sh"
     bash "../cutthecord/patches/branding/customdynamicicon.sh"
-}
+fi
 
-build(){
-    apktool b
-    jarsigner -keystore /home/mia/ctc.keystore -storepass $keystore_passwd dist/com.cutthecord.femboy-$ver.apk ctc
-}
-
-main() {
-    user_set
-    setup
-    emotes
-    patch
-    res
-    build
-}
-
-main "$@"
+apktool b
+jarsigner -keystore /home/mia/ctc.keystore -storepass $keystore_passwd dist/com.cutthecord.$CTCFORK-$ver.apk ctc
