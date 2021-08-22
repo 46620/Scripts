0b5b68cd980971eee6fc46ef482c64ec96e91921
46620 <46620osu@gmail.com>
2021-08-22 14:59:37 -0400

    Update Plex metadata scripts

--- plex/update-HAMA.sh
+++ plex/update-HAMA.sh
@@ -3,18 +3,18 @@
 # run with sudo, tested on ubuntu
 
 echo STOPPING PLEX
-systemctl stop plexmediaserver
+docker stop Plex
 
 echo UPDATING HAMA
-cd '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
+cd '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
 git reset --hard && git pull
-chown -R plex:plex '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
-chmod 775 -R '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
+chown -R plex:plex '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
+chmod 775 -R '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
 
 echo UPDATING Absolute Series Scanner
-wget -O '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners/Series/Absolute Series Scanner.py' https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py
-chown -R plex:plex '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners'
-chmod 775 -R '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners'
+wget -O '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners/Series/Absolute Series Scanner.py' https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py
+chown -R plex:plex '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners'
+chmod 775 -R '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners'
 
 echo STARTING PLEX
-systemctl restart plexmediaserver
+docker start Plex
--- plex/update-anilist.sh
+++ plex/update-anilist.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# run with sudo, tested on ubuntu
+# run with sudo, tested on ubuntu (running in docker container)
 
 echo STOPPING PLEX
-systemctl stop plexmediaserver
+docker stop Plex
 
 echo UPDATING anilist
-cd '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/anilist.bundle'
+cd '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/anilist.bundle'
 git reset --hard && git pull
-chown -R plex:plex '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/anilist.bundle'
-chmod 775 -R '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/anilist.bundle'
+chown -R plex:plex '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/anilist.bundle'
+chmod 775 -R '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/anilist.bundle'
 
 echo UPDATING Absolute Series Scanner
-wget -O '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners/Series/Absolute Series Scanner.py' https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py
-chown -R plex:plex '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners'
-chmod 775 -R '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners'
+wget -O '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners/Series/Absolute Series Scanner.py' https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py
+chown -R plex:plex '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners'
+chmod 775 -R '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners'
 
 echo STARTING PLEX
-systemctl restart plexmediaserver
+docker start Plex
