f1b46255fb1c027f18b795996138bd0db037e61f
46620 <46620osu@gmail.com>
2021-09-16 03:21:51 -0400

    These aren't needed for the docker container
    I think this might cause an issue so I'm gonna just don't

--- plex/update-HAMA.sh
+++ plex/update-HAMA.sh
@@ -8,12 +8,10 @@ docker stop Plex
 echo UPDATING HAMA
 cd '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
 git reset --hard && git pull
-chown -R plex:plex '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
 chmod 775 -R '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
 
 echo UPDATING Absolute Series Scanner
 wget -O '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners/Series/Absolute Series Scanner.py' https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py
-chown -R plex:plex '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners'
 chmod 775 -R '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners'
 
 echo STARTING PLEX
