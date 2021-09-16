4bd4c719e57052c8be9c4666861f081e65e86fc1
46620 <46620osu@gmail.com>
2021-09-16 03:22:20 -0400

    See previous commit

--- plex/update-anilist.sh
+++ plex/update-anilist.sh
@@ -8,12 +8,10 @@ docker stop Plex
 echo UPDATING anilist
 cd '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/anilist.bundle'
 git reset --hard && git pull
-chown -R plex:plex '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/anilist.bundle'
 chmod 775 -R '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Plug-ins/anilist.bundle'
 
 echo UPDATING Absolute Series Scanner
 wget -O '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners/Series/Absolute Series Scanner.py' https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py
-chown -R plex:plex '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners'
 chmod 775 -R '/opt/plexmediaserver/config/Library/Application Support/Plex Media Server/Scanners'
 
 echo STARTING PLEX
