cc3b833a0118e34106d1959d9c3477b1d75d3676
46620 <46620osu@gmail.com>
2021-09-21 12:27:48 -0400

    "borrow" some code from a fork

--- homebrew/packs/catalyst-ctr.sh
+++ homebrew/packs/catalyst-ctr.sh
@@ -6,28 +6,23 @@
 ###################
 # Fucking kill me #
 ###################
-mkdir -p {3ds,cias,luma/payloads} # saves me the slightest bit of time later
 export script_start=`pwd`
+
 ########
 # Luma #
 ########
 echo "Downloading Luma"
 curl -sLq -H "Authorization: token $ghtoken" https://api.github.com/repos/LumaTeam/Luma3DS/releases/latest | jq -r '.assets[0].browser_download_url' | wget -qi -
-unzip -q Luma3DSv*.zip;rm Luma3DSv*.zip
 
 #################
 # Homebrew apps #
 #################
-
 echo "Downloading FBI" # might swap this out for the fork ngl
 curl -sLq -H "Authorization: token $ghtoken" https://api.github.com/repos/Steveice10/FBI/releases/latest | jq -r '.assets[].browser_download_url' | wget -qi -
 rm -rf FBI.zip
 
 echo "Downloading Anemone"
 curl -sLq -H "Authorization: token $ghtoken" https://api.github.com/repos/astronautlevel2/Anemone3DS/releases/latest | jq -r '.assets[1].browser_download_url' | wget -qi -
-
-echo "Downloading ftpd"
-curl -sLq -H "Authorization: token $ghtoken" https://api.github.com/repos/mtheall/ftpd/releases/latest | jq -r '.assets[0].browser_download_url' | wget -qi -
 
 echo "Downloading Checkpoint"
 curl -sLq -H "Authorization: token $ghtoken" https://api.github.com/repos/FlagBrew/Checkpoint/releases/latest | jq -r '.assets[1].browser_download_url' | wget -qi -
@@ -40,53 +35,116 @@ curl -sLq -H "Authorization: token $ghtoken" https://api.github.com/repos/marioh
 
 echo "Downloading GodMode9"
 curl -sLq -H "Authorization: token $ghtoken" https://api.github.com/repos/d0k3/GodMode9/releases/latest | jq -r '.assets[].browser_download_url' | wget -qi -
-mkdir tmp;mv GodMode9*.zip tmp;cd tmp/;unzip -q *.zip;mv GodMode9.firm ../luma/payloads;cd ..;rm -rf tmp
 
 echo "Downloading DSP1"
 wget -q "https://github.com/zoogie/DSP1/releases/download/v1.0/DSP1.cia" # Not updated since 2017 so not gonna do an API call
 
-echo "Downloading crt-no-timeoffset" # Not updated since 2019 so not gonna do an API call
+echo "Downloading ctr-no-timeoffset" # Not updated since 2019 so not gonna do an API call
 wget -q "https://github.com/ihaveamac/ctr-no-timeoffset/releases/download/v1.1/ctr-no-timeoffset.3dsx"
 
+echo "Downloading universal-otherapp"
+wget -q "https://github.com/TuxSH/universal-otherapp/releases/download/v1.3.0/otherapp.bin" # I do not think this is gonna get many updates, if it starts updating a lot I'll use the api
+
+echo "Downloading boot9strap"
+wget -q "https://github.com/SciresM/boot9strap/releases/download/1.3/boot9strap-1.3.zip" # Has not been updated since 2017, should be no reason to call the api
+
+echo "Downloading SafeB9SInstaller"
+wget -q "https://github.com/d0k3/SafeB9SInstaller/releases/download/v0.0.7/SafeB9SInstaller-20170605-122940.zip" # Same reason as above
+
+echo "Downloading frogminer_save"
+wget -q "https://github.com/zoogie/Frogminer/releases/download/v1.0/Frogminer_save.zip" # same reason as above
+
+echo "Downloading b9sTool"
+wget -q "https://github.com/zoogie/b9sTool/releases/download/v6.0.1/release_6.0.1.zip" # same reason as above
+
+#####################
+# make some folders #
+#####################
+echo "Creating directories for each catalyst"
+mkdir -p {soundhax/3ds,soundhax/cias,soundhax/boot9strap,soundhax/luma/payloads}
+mkdir -p {fredtool/3ds,fredtool/cias,fredtool/boot9strap,fredtool/luma/payloads}
+mkdir -p {pichaxx/3ds,pichaxx/cias,pichaxx/boot9strap,pichaxx/luma/payloads}
+
+echo "Creating output dir"
+mkdir out
+
+####################
+# Do the unzipping #
+####################
+echo "Do the unzipping"
+unzip -q Luma3DSv*.zip;rm Luma3DSv*.zip
+mkdir tmp;mv GodMode9*.zip tmp;cd tmp/;unzip -q *.zip;mv GodMode9.firm ..; mv -f gm9 ..;cd ..;rm -rf tmp
+unzip -q boot9strap-1.3.zip;rm boot9strap-1.3.zip
+mkdir tmp;mv SafeB9SInstaller*.zip tmp;cd tmp/;unzip -q *.zip;mv SafeB9SInstaller.bin ..;cd ..;rm -rf tmp
+unzip -q Frogminer_save.zip;rm Frogminer_save.zip
+mkdir tmp;mv release_6.0.1.zip tmp;cd tmp/;unzip -q *.zip;mv release_6.0.1/boot.nds ..;cd ..;rm -rf tmp
+
 ################
-# Moving shit  #
+# yay soundhax #
 ################
-
-echo "Putting homebrew files in correct locations"
-mv *.cia cias
-mv *.3dsx 3ds
-mv 3ds/boot.3dsx $script_start
-
-#################
-# First Archive #
-#################
-echo "Making cleanSD zip archive"
-7z a -tzip "out/latest-cleanSD.zip" * > /dev/null
-cd out
-md5sum "latest-cleanSD.zip" > "latest-cleanSD.zip.md5"
+echo "Putting soundhax related files in correct locations"
+cp *.cia soundhax/cias
+cp *.3dsx soundhax/3ds
+mv soundhax/3ds/boot.3dsx soundhax/boot.3dsx
+cp otherapp.bin soundhax
+cp boot.firm soundhax/boot.firm
+cp GodMode9.firm soundhax/luma/payloads
+cp -r gm9 soundhax/gm9
+cp boot9strap.* soundhax/boot9strap
+cp SafeB9SInstaller.bin soundhax
+cd soundhax
+zip -r soundhax.zip * 
+mv soundhax.zip ../out
 cd ..
+rm -rf soundhax
 
-##############
-# browserhax #
-##############
-echo "Downloading browserhax files"
-wget -qO "arm11code.bin" "https://github.com/TuxSH/universal-otherapp/releases/download/v1.3.0/otherapp.bin" # I do not think this is gonna get many updates, if it starts updating a lot I'll use the api
-wget -qO "boot9strap.zip" "https://github.com/SciresM/boot9strap/releases/download/1.3/boot9strap-1.3.zip" # Has not been updated since 2017, should be no reason to call the api
-wget -qO "SafeB9SInstaller.zip" "https://github.com/d0k3/SafeB9SInstaller/releases/download/v0.0.7/SafeB9SInstaller-20170605-122940.zip" # Same reason as above
-unzip -q boot9strap.zip
-rm boot9strap.zip
-mkdir boot9strap
-mv boot9strap.fir* boot9strap
-mkdir tmp;mv SafeB9SInstaller.zip tmp;cd tmp;unzip -q SafeB9SInstaller.zip; mv SafeB9SInstaller.bin ..;cd ..;rm -rf tmp
-
-######################
-# browserhax archive #
-######################
-echo "Making browserhax archive"
-7z a -tzip "out/latest-browserhax.zip" * -x\!out > /dev/null
-cd out
-md5sum "latest-browserhax.zip" > "latest-browserhax.zip.md5"
+####################
+# methax? pichaxx? #
+####################
+echo "Putting pichaxx related files in correct locations"
+cp *.cia pichaxx/cias
+cp *.3dsx pichaxx/3ds
+mv pichaxx/3ds/boot.3dsx pichaxx/boot.3dsx
+cp otherapp.bin pichaxx
+cp boot.firm pichaxx/boot.firm
+cp GodMode9.firm pichaxx/luma/payloads
+cp -r gm9 pichaxx/gm9
+cp boot9strap.* pichaxx/boot9strap
+cp SafeB9SInstaller.bin pichaxx
+cd pichaxx
+zip -r pichaxx.zip * 
+mv pichaxx.zip ../out
 cd ..
+rm -rf pichaxx
+
+###################
+# fwedtool is old #
+###################
+echo "Putting fredtool related files in correct locations"
+mv *.cia fredtool/cias
+mv *.3dsx fredtool/3ds
+mv fredtool/3ds/boot.3dsx fredtool/boot.3dsx
+mv otherapp.bin fredtool
+mv boot.firm fredtool/boot.firm
+mv GodMode9.firm fredtool/luma/payloads
+mv gm9 fredtool/gm9
+mv boot9strap.* fredtool/boot9strap
+mv private fredtool/
+mv SafeB9SInstaller.bin fredtool
+mv boot.nds fredtool
+cd fredtool
+zip -r fredtool.zip * 
+mv fredtool.zip ../out
+cd ..
+rm -rf fredtool
+
+###########
+# md5sums #
+###########
+for cum in out/*
+do
+    md5sum "$cum" > "$cum.md5"
+done
 
 ###########
 # Credits #
@@ -95,4 +153,5 @@ cd ..
 # My friend Logan for being in voice with my while not doing his school work
 # Will Stetson for making the music I am listening to while doing this https://www.youtube.com/watch?v=8daZtdrFmBY
 # My friend Brandon's DS that had a broken start button making it so I had to use browserhax and add the second zip file
+# Lifehacker for copying this script and adding a few other methods (which I then stole)
 # My boyfriend for letting me use him as a pillow a few times a week to keep me alive
