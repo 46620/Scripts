20a37530c4461030622f7f22929c83d39c231e20
46620 <46620osu@gmail.com>
2022-03-06 22:17:50 -0500

    [ffmpeg] automation is poggers

--- video-scripts/vtrack-name.sh
+++ video-scripts/vtrack-name.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-# this is mostly to change file hashes so they're slightly different from any public file
+#set -x
+set -e
+cd Anime
+folders=( "$D"* )
 
-files=()
-while IFS=  read -r -d $'\0'; do
-    files+=("$REPLY")
-done < <(find . -iname "*.mkv" -print0 | sed 's@./@@g' | sed 's/.mkv//g')
-
-for cum in "${files[@]}"
+for cum in "${folders[@]}"
 do
-    echo $cum
+    cd "$cum"
+    folders2=()
+    folders2=( "$D"* )
+    for cum2 in "${folders2[@]}"
+    do
+        cd "$cum2"
+        mkdir tmp
+        files=()
+            while IFS=  read -r -d $'\0'; do
+            files+=("$REPLY")
+            done < <(find . -iname "*.mkv" -print0 | sed 's@./@@g' | sed 's/.mkv//g')
+        echo "${files[@]}"
+        for cum in "${files[@]}"
+            do
+                echo $cum
+                ffmpeg -i "$cum.mkv" -map 0 -c copy -metadata:s:v:0 title="$cum" "tmp/$cum.mkv"
+            done
+        mv tmp/* .
+        rm -r tmp
+        cd ..
+    done
+    cd ..
 done
