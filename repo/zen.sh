ac932e4d8f9aad130610e2787c46b31f62b69301
46620 <46620osu@gmail.com>
2022-05-22 14:20:40 -0400

    forgot that some things don't have wget for some reason

--- repo/zen.sh
+++ repo/zen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script is meant for use with auto patching the PKGBUILD of linux-zen to add a KVM patch, and disable building kernel docs
 
-wget "https://raw.githubusercontent.com/46620/patches/master/kvm.patch"
+curl -o kvm.patch "https://raw.githubusercontent.com/46620/patches/master/kvm.patch"
 gpg --keyserver hkps://keys.openpgp.org --recv-key 3B94A80E50A477C7 # key fails normally, this is needed only for first run, but won't break the script if ran multiple times.
 sed -i 's@pkgname=("$pkgbase" "$pkgbase-headers" "$pkgbase-docs")@pkgname=("$pkgbase" "$pkgbase-headers")@g' PKGBUILD
 sed -i 's@make htmldocs@@g' PKGBUILD
