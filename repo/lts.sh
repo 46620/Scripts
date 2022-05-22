17019f487563602dd958c99011343ea45b11cdd2
46620 <46620osu@gmail.com>
2022-05-22 15:50:16 -0400

    [repo] LTS for the server

---
+++ repo/lts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script is meant for use with auto patching the PKGBUILD of linux-zen to add a KVM patch, and disable building kernel docs
+
+sed -i 's@pkgname=("$pkgbase" "$pkgbase-headers" "$pkgbase-docs")@pkgname=("$pkgbase" "$pkgbase-headers")@g' PKGBUILD
+sed -i 's@make htmldocs@@g' PKGBUILD
+
+ed PKGBUILD <<EOF
+68i
+  make LSMOD=/etc/modprobed.db localmodconfig
+.
+w
+q
+EOF
+
+updpkgsums
+
