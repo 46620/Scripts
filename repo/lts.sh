c0d455ccb0fe6be9c0fad201ff679a4caf5e3f64
46620 <46620osu@gmail.com>
2022-05-22 17:04:19 -0400

    [repo] rename the kernel so it doesn't break official LTS

--- repo/lts.sh
+++ repo/lts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script is meant for use with auto patching the PKGBUILD of linux-zen to add a KVM patch, and disable building kernel docs
 
+sed -i 's@pkgbase=linux-lts@pkgbase=linux-lts-server@g' PKGBUILD
 sed -i 's@pkgname=("$pkgbase" "$pkgbase-headers" "$pkgbase-docs")@pkgname=("$pkgbase" "$pkgbase-headers")@g' PKGBUILD
 sed -i 's@make htmldocs@@g' PKGBUILD
 
