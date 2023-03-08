37794cac230b6d63d2bea4027cd3297ec4a4b8c2
46620 <46620osu@gmail.com>
2023-03-08 14:21:57 -0500

    no more windows ffmpeg scripts

--- windows/ffmpeg/mp3.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:: This script converts any file shoved into it to mp3 where the script is called. This is meant for drag and drop but can be put into PATH and called anytime
-
-ffmpeg -i %1 "%~nx1.mp3"
