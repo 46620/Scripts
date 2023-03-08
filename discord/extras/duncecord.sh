a8a43b2caef45b620d593cba6bf1f14a3d5b4e89
46620 <46620osu@gmail.com>
2023-03-08 14:52:05 -0500

    no more discord stuff at all

--- discord/extras/duncecord.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Helper script for my discord patch
-
-if [ "$(loginctl show-session "$XDG_SESSION_ID" -p Type --value)" = wayland ]; then
-    /usr/bin/chromium --enable-features=UseOzonePlatform --ozone-platform=wayland --app=https://canary.discord.com
-else
-    /usr/bin/chromium --app=https://canary.discord.com
-fi
