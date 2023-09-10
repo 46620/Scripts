4bf161de0224cdf8b18cd0e41733e441fa761c54
46620 <46620osu@gmail.com>
2023-09-10 13:13:46 -0400

    no more fnf

--- fnf/builder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# remember that one comment that said "not fixing after 1am?" well I started playing the fuckiung game
-
-lime build linux
-#lime build mac
-#lime build android
-#lime build html5
-#lime build windows
